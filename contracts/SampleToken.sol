72358c984209491d92e1c9fe29306ae4811a005e
Seungwon Go <seungwon.go@gmail.com>
2018-09-17 08:04:10 +0900

    [UPDATE] — 토큰 컨트랙트 구현, 테스트케이스 작성

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -1,12 +1,16 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/StandardBurnableToken.sol";
 
-contract SampleToken is DetailedERC20, StandardToken {
+contract SampleToken is DetailedERC20, MintableToken, PausableToken, StandardBurnableToken {
   constructor(string _name, string _symbol, uint8 _decimals, uint256 _totalSupply)
   DetailedERC20(_name, _symbol, _decimals)
   public {
-    totalSupply_ = _totalSupply;
+    totalSupply_ = _totalSupply * 10**uint(decimals);
+    balances[owner] = totalSupply_;
+    emit Transfer(address(0), owner, totalSupply_);
   }
 }
